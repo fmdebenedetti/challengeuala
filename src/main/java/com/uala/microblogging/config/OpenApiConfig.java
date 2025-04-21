87d0d842158d530f16af28586f9c92dabb9342a9
fmdebenedetti <flaviodebenedetti@gmail.com>
2025-04-21 16:42:31 -0300

    Agrego open api config

---
+++ src/main/java/com/uala/microblogging/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.uala.microblogging.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Microblogging API")
+                        .description("API REST para el servicio de microblogging")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Flavio de Benedetti")
+                                .email("flaviodebenedetti@gmail.com")));
+    }
+}
